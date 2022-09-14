fbc9ac3aa614d45230024aac74378c8ef68fc449
Munim Munna <6266677+monim67@users.noreply.github.com>
2022-09-14 21:02:17 +0600

    Fix build by updating tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,9 +7,9 @@ from pathlib import Path
 testing_assets = Path(__file__).parent / "assets"
 
 
-def copy_project(project_name: str, destination_dir: Path):
+def copy_project(project_name: str, destination_dir: Path) -> Path:
     package_path = testing_assets / project_name
-    shutil.copytree(package_path, destination_dir, dirs_exist_ok=True)
+    return shutil.copytree(package_path, destination_dir / "project")
 
 
 def execute_update_version_command(project_dir: Path, new_version: str):
@@ -25,25 +25,25 @@ def execute_update_version_command(project_dir: Path, new_version: str):
 
 def test_project_with_instructions(tmp_path: Path):
     new_version = "1.0.0"
-    copy_project("project_with_instructions", tmp_path)
-    result = execute_update_version_command(tmp_path, new_version)
+    project_dir = copy_project("project_with_instructions", tmp_path)
+    result = execute_update_version_command(project_dir, new_version)
     assert new_version in result.stdout
     for file in (
-        tmp_path / "test_package/__init__.py",
-        tmp_path / "test_package/version.py",
-        tmp_path / "README.md",
+        project_dir / "test_package/__init__.py",
+        project_dir / "test_package/version.py",
+        project_dir / "README.md",
     ):
         assert new_version in file.read_text()
 
 
 def test_project_with_replacements(tmp_path: Path):
     new_version = "1.0.0"
-    copy_project("project_with_replacements", tmp_path)
-    result = execute_update_version_command(tmp_path, new_version)
+    project_dir = copy_project("project_with_replacements", tmp_path)
+    result = execute_update_version_command(project_dir, new_version)
     assert new_version in result.stdout
     for file in (
-        tmp_path / "test_package/__init__.py",
-        tmp_path / "test_package/version.py",
-        tmp_path / "README.md",
+        project_dir / "test_package/__init__.py",
+        project_dir / "test_package/version.py",
+        project_dir / "README.md",
     ):
         assert new_version in file.read_text()
